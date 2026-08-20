640ef225e84fd9682f8426dbf53d0d9ac961189e
Arsh Mishra <mishraarsh500@gmail.com>
2026-08-20 16:46:57 +0530

    My first commit

---
+++ python2.py
@@ -0,0 +1,106 @@
+#a,b,c = 10,20,30
+#print(a,b,c)
+#a,b,c = 10,20
+#print(a,b,c)
+#a,b = 10,20,30
+#print(a,b,c)
+
+#a = b = c = 10
+#print(a,b,c)
+
+'''a = 20
+print("first assinged value", a)
+a = 40
+print("variable is re-intialised, now value is:", a)
+print(type(a))'''
+
+'''a = 2e2
+b = 2E2
+c = 2e3
+print(a)
+print(b)
+print(c)
+print(type(a))'''
+
+'''a = 3+5j
+b = 2-5.5j
+c = 3+10.5j
+print(a)
+print(b)
+print(c)
+print()
+print(a+b)
+print(b+c)
+print(a+c)'''
+
+'''a = True
+b = " "
+print(a)
+print(bool(b))'''
+
+'''a = None
+print(a)
+print((type(a)))'''
+
+#a = 'Shyaam laudelaal'
+#b = "Shyaam laudelaal"
+#c = '''rudra betichod
+#is a 
+#big boob'''
+#d = """Shyaam laudelaal 
+#is a 
+#big boob"""'''
+
+'''x = [10, 20, 100, 40, 15]
+y = bytes(x)
+print(type(y))
+print(y[0])
+print(y[1])
+print(y[2])
+print(y[3])
+print(y[4])'''
+
+'''x = [10, 20, 100, 40, 15]
+y = bytes(x)
+for a in y:
+    print(a)'''
+
+'''x = [10, 20, 300, 40, 15]
+y = bytes(x)'''
+
+'''x = [10, 20, 100, 40, 15]
+y = bytes(x)
+y[0] = 30'''
+
+'''a = range(5)
+print(a)
+for x in a:
+    print(x)'''
+
+'''a = range(1,10)
+print(a)
+for x in a:
+    print(x)'''
+
+'''a = range(1,7,2)
+print(a)
+for x in a:
+    print(x)'''
+
+'''a = range(10,2)
+print(a)
+for x in a:
+    print(x)'''
+
+'''a = 5
+n = float(a)
+print(n)
+print(type(n))
+a = 5.9
+n = int(a)
+print(n)
+print(type(n))
+a = 5
+n = str(a)
+print(n)
+print(type(n))'''
