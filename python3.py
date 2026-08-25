c34980a388f3b1aaf0177b593afd6c0625ab7711
Arsh Mishra <mishraarsh500@gmail.com>
2026-08-25 11:54:40 +0530

    fifth commit

---
+++ python3.py
@@ -0,0 +1,36 @@
+'''a="acbd"+str(12)
+print(a)
+b=12+int("abcd")
+print(b)'''
+
+'''a=5.9
+n=int(a)
+print(n)
+print(type(n))'''
+
+#OPERATORS
+
+'''a=4
+b=3
+c=a+b
+print(c)'''
+
+'''a=10
+b=20
+c= (a if a<b else b)+89
+print(c)'''
+
+'''x=10
+y=-3
+z=90
+s=(x if x<z else z) if x<y else (y if y<z else z)
+print(s)'''
+
+'''x=10
+y=-3
+z=90
+w=100
+s= ((x if x<w else w) if x<y else (y if y<w else w)) if x<y else ((y if y<w else w) if y<z else (z if z<w else w))
+print(s)'''
+
+
